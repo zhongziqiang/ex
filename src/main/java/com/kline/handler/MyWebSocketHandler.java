36f2152055379dc6603c4dfccda95141bb775543
zhongziqiang <359831859@qq.com>
2025-04-25 18:42:45 +0800

    添加websocket支持,获取币安测试网k线,以及扩展

---
+++ src/main/java/com/kline/handler/MyWebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.kline.handler;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        System.out.println("New connection: " + session.getId());
+        session.sendMessage(new TextMessage("Welcome to WebSocket server!"));
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("Received message: " + message.getPayload());
+
+        // 广播消息给所有客户端
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(new TextMessage(
+                        "Echo: " + message.getPayload()));
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        System.out.println("Connection closed: " + session.getId());
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        System.err.println("Transport error: " + exception.getMessage());
+        session.close(CloseStatus.SERVER_ERROR);
+    }
+}
